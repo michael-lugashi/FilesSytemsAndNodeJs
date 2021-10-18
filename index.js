560172422bc2e7f95a22edbe49aa1fe1b982f65d
michael-lugashi <89575620+michael-lugashi@users.noreply.github.com>
2021-10-18 21:07:46 +0300

    openFolder function, and changed name of maze
    I changed the name of my maze from maze-example to maze
    Then I made the openFolder function which allows me to view all the chests in the folder.

---
+++ index.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const fs = require('fs');
+
+function openFolder(folder){
+    const isFile = (fileName) => fs.lstatSync(fileName).isFile();
+    let chests = fs
+      .readdirSync(folder)
+      .map((fileName) => path.join(folder, fileName))
+      .filter(isFile);
+    console.log(chests)
+    return chests
+}
+
+
+
+openFolder('.\\maze');
+
+
+// try {
+//     const data = fs.readFileSync(`.\\maze-example\\${}, 'utf8'`)
+//     console.log(data)
+//   } catch (err) {
+//     // console.error(err)
+//   }
+
+// process.stdin.on("data", dataBuffer => {
+//     const input = dataBuffer.toString().trim();
+//     try {
+//         // const content = fs.readdirSync(`${__dirname}\\${input}`);
+//         const content = fs.readdirSync(path.relative(__dirname, input));
+//         process.stdout.write(content.join("\n"));
+//         process.exit(0);
+//     } catch(e) {
+//         process.stderr.write(e.toString());
+//         process.exit(1);
+//     }
+// });
+
+// function drawMapSync(currentRoomPath) {}
