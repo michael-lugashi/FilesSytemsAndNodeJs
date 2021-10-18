430823cda3e7350b7ec3be63a6f16ae320a743d1
michael-lugashi <89575620+michael-lugashi@users.noreply.github.com>
2021-10-18 21:13:18 +0300

    fixed indentation

--- index.js
+++ index.js
@@ -1,45 +1,44 @@
 const path = require('path');
 const fs = require('fs');
 
-function openFolder(folder){
-    const isFile = (fileName) => fs.lstatSync(fileName).isFile();
-    let chests = fs
-      .readdirSync(folder)
-      .map((fileName) => path.join(folder, fileName))
-      .filter(isFile);
-    console.log(chests)
-    return chests
+function openFolder(folder) {
+  const isFile = (fileName) => fs.lstatSync(fileName).isFile();
+  let chests = fs
+    .readdirSync(folder)
+    .map((fileName) => path.join(folder, fileName))
+    .filter(isFile);
+  console.log(chests);
+  return chests;
 }
 
-let i = 0
+let i = 0;
 findTreasureSync(openFolder('.\\maze'));
 function findTreasureSync(roomPath) {
-    // console.log(roomPath[i])
-    try {
-      //   console.log(roomPath[i])
-      const data = fs.readFileSync(roomPath[i], 'utf8');
-      // console.log(data);
-      // openChestSync(data);
-      console.log('');
-      i = 0;
-      let chest = (openChestSync(data))
-      if (chest.treasure === true) {
-          console.log('congrats')
-          return
-      }
-      chest = chest.clue
-  
-      findTreasureSync(openFolder(chest))
-    } catch (err) {
-      i++;
-      console.log('didnt');
-      findTreasureSync(roomPath);
+
+  try {
+    //   console.log(roomPath[i])
+    const data = fs.readFileSync(roomPath[i], 'utf8');
+    // console.log(data);
+    // openChestSync(data);
+    console.log('');
+    i = 0;
+    let chest = openChestSync(data);
+    if (chest.treasure === true) {
+      console.log('congrats');
+      return;
     }
+    chest = chest.clue;
+
+    findTreasureSync(openFolder(chest));
+  } catch (err) {
+    i++;
+    console.log('didnt');
+    findTreasureSync(roomPath);
   }
-  
+}
+
 function openChestSync(chestPath) {
-    console.log(JSON.parse(chestPath))
-    return JSON.parse(chestPath)
+  console.log(JSON.parse(chestPath));
+  return JSON.parse(chestPath);
 }
 openFolder('.\\maze');
-
