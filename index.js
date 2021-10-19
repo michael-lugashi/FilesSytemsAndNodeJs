99c65e8120ab0c1a121aa078b827c70ef7b16989
michael-lugashi <89575620+michael-lugashi@users.noreply.github.com>
2021-10-19 11:48:25 +0300

    got rid of global variable
    I got rid of my global variable. My global variable enabled me to do a loop on all the chests in a room. I just put the loop inside my find treasure function instead of keeping it on the outside.

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 'use strict';
 const path = require('path');
 const fs = require('fs');
+findTreasureSync('.\\maze');
+
 // this function opens a folder
 function openFolder(folder) {
   drawMapSync(folder);
   return fs.readdirSync(folder).map((fileName) => path.join(folder, fileName));
 }
-
-let i = 0;
-findTreasureSync(openFolder('.\\maze'));
 // this function accepts an array of paths and goes over them until one works then checks the paths in that path.
-function findTreasureSync(chestPaths) {
-  try {
-    const chestContent = openChestSync(chestPaths[i]);
-    i = 0;
-    if (chestContent.treasure === true) {
-      return;
+function findTreasureSync(roomPath) {
+  const chestPaths = openFolder(roomPath);
+  for (const chestPath of chestPaths) {
+    try {
+      const chestContent = openChestSync(chestPath);
+      if (chestContent.treasure === true) {
+        break;
+      }
+      findTreasureSync(chestContent.clue);
+    } catch {
+      continue;
     }
-
-    findTreasureSync(openFolder(chestContent.clue));
-
-  } catch {
-    i++;
-    findTreasureSync(chestPaths);
+    return;
   }
 }
 // lets us check the contents of the chest
